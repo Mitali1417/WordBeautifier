92bf105175aca39b0a2fb75ad519b46df36230d0
Mitali <mitali9205@gmail.com>
2023-01-28 15:10:56 +0530

    Updates

---
+++ precache-manifest.a982e57b13c375fd74b5dee17bc1a0b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ad1dd8743ae09faeb39",
+    "url": "/WordBeautifier/static/css/main.476a2fe4.chunk.css"
+  },
+  {
+    "revision": "1ad1dd8743ae09faeb39",
+    "url": "/WordBeautifier/static/js/main.1ad1dd87.chunk.js"
+  },
+  {
+    "revision": "8bcc787db593634d27ed",
+    "url": "/WordBeautifier/static/js/1.8bcc787d.chunk.js"
+  },
+  {
+    "revision": "5056e11181cbe38676c9",
+    "url": "/WordBeautifier/static/js/runtime~main.5056e111.js"
+  },
+  {
+    "revision": "779960b2e1bf778042080c40c7105be7",
+    "url": "/WordBeautifier/index.html"
+  }
+];
